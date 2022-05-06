59df0a6adb0688bbd7c0a2cb625d3e06bddcd133
hrajpoot-js <65027195+hrajpoot-js@users.noreply.github.com>
2022-05-06 14:39:21 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,46 @@
 #include <vector>
+#include <bits/stdc++.h>
 
-namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+class Stats
+{
+    public:
+        double min;
+        double max;
+        double average;
+
+    Stats(double imin, double imax, double iaverage): min(imin), max(imax), average(iaverage) {}
+};
+
+namespace Statistics
+{
+    Stats ComputeStatistics(const std::vector<double>& input);
 }
+
+
+class IAlerter
+{
+    public:
+        bool emailSent = false;
+        bool ledGlows = false;
+};
+
+class EmailAlert : public IAlerter
+{
+};
+
+class LEDAlert : public IAlerter
+{
+};
+
+class StatsAlerter
+{
+    private:
+    const float aMaxThreshold;
+    std::vector<IAlerter*> aAlerters;
+
+    public:
+        StatsAlerter(const float iMaxThreshold, std::vector<IAlerter*> iAlerters):
+                aMaxThreshold(iMaxThreshold), aAlerters(iAlerters) { }
+
+    void checkAndAlert(std::vector<float> iInput);
+};
